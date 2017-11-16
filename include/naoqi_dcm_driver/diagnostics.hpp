fd6e648aeef392e4e02359958b0262a1115d7b77
Natalia Lyubova <nlyubova@aldebaran.com>
2017-11-16 15:35:51 +0100

    Rebase from 'rpm/kinetic/naoqi_dcm_driver'

--- include/naoqi_dcm_driver/diagnostics.hpp
+++ include/naoqi_dcm_driver/diagnostics.hpp
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2016 SoftBank Robotics Europe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
 #ifndef DIAGNOSTICS_H
 #define DIAGNOSTICS_H
 
@@ -49,6 +66,9 @@ private:
 
   /** joints names */
   std::vector <std::string> joints_all_names_;
+
+  /** joints electric current */
+  std::vector <float> joints_current_;
 
   /** all the keys to check. It is a concatenation of
    * temperatures_keys, stiffness_keys, current_keys */
