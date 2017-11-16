4b83ab799a01461de8880eb6beeafb2a98974393
Natalia Lyubova <nlyubova@aldebaran.com>
2017-11-16 15:30:03 +0100

    Rebase from 'release/jade/naoqi_dcm_driver'

--- src/robot.cpp
+++ src/robot.cpp
@@ -19,6 +19,8 @@
 
 #include <diagnostic_msgs/DiagnosticArray.h>
 
+#include <XmlRpcValue.h>
+
 #include "naoqi_dcm_driver/robot.hpp"
 #include "naoqi_dcm_driver/tools.hpp"
 
@@ -40,7 +42,7 @@ Robot::Robot(qi::SessionPtr session):
                joint_precision_(0.1),
                odom_frame_("odom"),
                use_dcm_(false),
-               use_cmd_vel_(false)
+               stiffness_value_(0.9f)
 {
 }
 
@@ -52,17 +54,22 @@ Robot::~Robot()
 void Robot::stopService() {
   ROS_INFO_STREAM(session_name_ << " stopping the service...");
 
-  //reset stiffness for arms if using DCM to prevent its concurrence with ALMotion
-  if(use_dcm_)
-    motion_->setStiffnessArms(0.0f, 1.0f);
+  if (motion_)
+  {
+    /* reset stiffness for arms if using DCM
+     * to prevent its concurrence with ALMotion */
+    if(use_dcm_)
+      motion_->setStiffnessArms(0.0f, 2.0f);
 
-  //going to rest
-  if (motor_groups_.size() == 1)
-    if (motor_groups_[0] == "Body")
-      motion_->rest();
+    //going to rest
+    if (motor_groups_.size() == 1)
+      if (motor_groups_[0] == "Body")
+        motion_->rest();
 
-  //set stiffness for the whole body
-  setStiffness(0.0f);
+    //set stiffness for the whole body
+    //setStiffness(0.0f);
+    motion_->setStiffnessArms(0.0f, 2.0f);
+  }
 
   is_connected_ = false;
 
@@ -73,31 +80,33 @@ void Robot::stopService() {
   }
 }
 
-bool Robot::initializeControllers(const std::vector <std::string> &joints_names)
+bool Robot::initializeControllers(const std::vector <std::string> &joints)
 {
-  int joints_nbr = joints_names.size();
+  int joints_nbr = joints.size();
 
   // Initialize Controllers' Interfaces
-  joint_angles_.reserve(joints_nbr);
-  joint_velocities_.reserve(joints_nbr);
-  joint_efforts_.reserve(joints_nbr);
-  joint_commands_.reserve(joints_nbr);
+  hw_angles_.reserve(joints_nbr);
+  hw_velocities_.reserve(joints_nbr);
+  hw_efforts_.reserve(joints_nbr);
+  hw_commands_.reserve(joints_nbr);
 
-  joint_angles_.resize(joints_nbr);
-  joint_velocities_.resize(joints_nbr);
-  joint_efforts_.resize(joints_nbr);
-  joint_commands_.resize(joints_nbr);
+  hw_angles_.resize(joints_nbr);
+  hw_velocities_.resize(joints_nbr);
+  hw_efforts_.resize(joints_nbr);
+  hw_commands_.resize(joints_nbr);
 
   try
   {
     for(int i=0; i<joints_nbr; ++i)
     {
-      hardware_interface::JointStateHandle state_handle(joints_names.at(i), &joint_angles_[i],
-                                                        &joint_velocities_[i], &joint_efforts_[i]);
+      hardware_interface::JointStateHandle state_handle(joints.at(i),
+                                                        &hw_angles_[i],
+                                                        &hw_velocities_[i],
+                                                        &hw_efforts_[i]);
       jnt_state_interface_.registerHandle(state_handle);
 
-      hardware_interface::JointHandle pos_handle(jnt_state_interface_.getHandle(joints_names.at(i)),
-                                                 &joint_commands_[i]);
+      hardware_interface::JointHandle pos_handle(jnt_state_interface_.getHandle(joints.at(i)),
+                                                 &hw_commands_[i]);
       jnt_pos_interface_.registerHandle(pos_handle);
     }
 
@@ -113,14 +122,14 @@ bool Robot::initializeControllers(const std::vector <std::string> &joints_names)
   return true;
 }
 
-
 // The entry point from outside
 bool Robot::connect()
 {
   is_connected_ = false;
 
   // Load ROS Parameters
-  loadParams();
+  if (!loadParams())
+    return false;
 
   // Initialize DCM Wrapper
   if (use_dcm_)
@@ -154,22 +163,36 @@ bool Robot::connect()
   if (use_dcm_)
     motion_->manageConcurrence();
 
-  //read joints names that will be controlled
-  std::vector <std::string> joints_names = motion_->getBodyNamesFromGroup(motor_groups_);
-  ignoreMimicJoints(&joints_names);
-  ROS_INFO_STREAM("The following joints are controlled: " << print(joints_names));
+  //read Naoqi joints names that will be controlled
+  qi_joints_ = motion_->getBodyNamesFromGroup(motor_groups_);
+  if (qi_joints_.empty())
+    ROS_ERROR("Controlled joints are not known.");
+  //define HW joints if empty
+  if (hw_joints_.empty())
+  {
+    ROS_INFO_STREAM("Initializing the HW controlled joints with Naoqi joints.");
+    hw_joints_.reserve(qi_joints_.size());
+    copy(qi_joints_.begin(), qi_joints_.end(), back_inserter(hw_joints_));
+  }
+  ROS_INFO_STREAM("HW controlled joints are : " << print(hw_joints_));
+
+  ignoreMimicJoints(&qi_joints_);
+  ROS_INFO_STREAM("Naoqi controlled joints are : " << print(qi_joints_));
+  qi_commands_.reserve(qi_joints_.size());
+  qi_commands_.resize(qi_joints_.size(), 0.0);
 
   //initialise Memory, Motion, and DCM classes with controlled joints
-  memory_->init(joints_names);
-  motion_->init(joints_names);
+  memory_->init(qi_joints_);
+  motion_->init(qi_joints_);
   if (use_dcm_)
-    dcm_->init(joints_names);
+    dcm_->init(qi_joints_);
+
+  hw_enabled_ = checkJoints();
 
   //read joints names to initialize the joint_states topic
   joint_states_topic_.header.frame_id = "base_link";
-  joint_states_topic_.name = motion_->getBodyNames("Body"); //should be Body=JointActuators+Wheels
+  joint_states_topic_.name = motion_->getBodyNames("Body"); //Body=JointActuators+Wheels
   joint_states_topic_.position.resize(joint_states_topic_.name.size());
-  keys_positions_all_ = memory_->initMemoryKeys(joint_states_topic_.name);
 
   //read joints names to initialize the diagnostics
   std::vector<std::string> joints_all_names = motion_->getBodyNames("JointActuators");
@@ -182,12 +205,21 @@ bool Robot::connect()
   subscribe();
 
   // Turn Stiffness On
-  if (!setStiffness(1.0f))
+  if (!setStiffness(stiffness_value_))
     return false;
 
   // Initialize Controller Manager and Controllers
-  manager_ = new controller_manager::ControllerManager( this, *nhPtr_);
-  if(!initializeControllers(joints_names))
+  try
+  {
+    manager_ = new controller_manager::ControllerManager( this, *nhPtr_);
+  }
+  catch(const ros::Exception& e)
+  {
+    ROS_ERROR("Could not initialize controller manager!\n\tTrace: %s", e.what());
+    return false;
+  }
+
+  if(!initializeControllers(hw_joints_))
     return false;
 
   ROS_INFO_STREAM(session_name_ << " module initialized!");
@@ -197,8 +229,7 @@ bool Robot::connect()
 void Robot::subscribe()
 {
   // Subscribe/Publish ROS Topics/Services
-  if (use_cmd_vel_)
-    cmd_vel_sub_ = nhPtr_->subscribe(prefix_+"cmd_vel", topic_queue_, &Robot::commandVelocity, this);
+  cmd_moveto_sub_ = nhPtr_->subscribe(prefix_+"cmd_moveto", 1, &Robot::commandVelocity, this);
 
   diag_pub_ = nhPtr_->advertise<diagnostic_msgs::DiagnosticArray>(prefix_+"diagnostics", topic_queue_);
 
@@ -208,7 +239,7 @@ void Robot::subscribe()
   joint_states_pub_ = nhPtr_->advertise<sensor_msgs::JointState>("/joint_states", topic_queue_);
 }
 
-void Robot::loadParams()
+bool Robot::loadParams()
 {
   ros::NodeHandle nh("~");
   // Load Server Parameters
@@ -219,11 +250,17 @@ void Robot::loadParams()
   nh.getParam("JointPrecision", joint_precision_);
   nh.getParam("OdomFrame", odom_frame_);
   nh.getParam("use_dcm", use_dcm_);
+
+  if (nh.hasParam("max_stiffness"))
+    nh.getParam("max_stiffness", stiffness_value_);
+
   if (use_dcm_)
-    ROS_WARN_STREAM("Please, be carefull! You have chosen to control the robot based on DCM. "
-                    << "This leads to concurrence between DCM and ALMotion and "
-                    << "it can cause shaking the robot. If it starts shaking, stop the node, "
-                    << "for example by pressing Ctrl+C");
+    ROS_WARN_STREAM("Please, be carefull! "
+                    << "You have chosen to control the robot based on DCM. "
+                    << "It leads to concurrence between DCM and ALMotion, and "
+                    << "it can cause shaking the robot. "
+                    << "If the robot starts shaking, stop the node (Ctrl+C). "
+                    << "Use either ALMotion or stop ALMotion and use DCM only.");
 
   //set the prefix for topics
   nh.getParam("Prefix", prefix_);
@@ -231,19 +268,41 @@ void Robot::loadParams()
     if (prefix_.at(prefix_.length()-1) != '/')
       prefix_ += "/";
 
-  //set the motors groups to control
-  std::string tmp="";
-  nh.getParam("motor_groups", tmp);
-  boost::erase_all(tmp, " ");
-  boost::split (motor_groups_, tmp, boost::is_any_of(","));
-  if (motor_groups_.size() == 1)
-    if (motor_groups_[0].empty())
-      motor_groups_.clear();
-  if (motor_groups_.size() == 0)
+  //read HW controllers names
+  XmlRpc::XmlRpcValue params_pepper_dcm;
+  nh.getParam("pepper_dcm", params_pepper_dcm);
+  if (params_pepper_dcm.getType() != XmlRpc::XmlRpcValue::TypeStruct)
+    ROS_ERROR("Please ensure that the list of controllers is TypeStruct");
+
+  XmlRpc::XmlRpcValue::ValueStruct::const_iterator it=params_pepper_dcm.begin();
+  for (; it != params_pepper_dcm.end(); ++it)
+  {
+    XmlRpc::XmlRpcValue::ValueStruct::const_iterator it2 = params_pepper_dcm[it->first].begin();
+    for (; it2 != params_pepper_dcm[it->first].end(); ++it2)
+    {
+      std::string param = (std::string)(it2->first);
+      if (param.compare("joints") == 0)
+      {
+        XmlRpc::XmlRpcValue params_joints = params_pepper_dcm[it->first][it2->first];
+        if (params_joints.getType() == XmlRpc::XmlRpcValue::TypeArray)
+        {
+          xmlToVector(params_joints, &hw_joints_);
+          continue;
+        }
+      }
+    }
+  }
+
+  //define the motors groups to control
+  std::string motor_groups_temp = "";
+  nh.getParam("motor_groups", motor_groups_temp);
+  motor_groups_ = toVector(motor_groups_temp);
+  if (motor_groups_.empty())
   {
     motor_groups_.push_back("LArm");
     motor_groups_.push_back("RArm");
   }
+  return true;
 }
 
 void Robot::run()
@@ -265,16 +324,27 @@ void Robot::controllerLoop()
 
     stiffness_pub_.publish(stiffness_);
 
-    readJoints();
-
     if (!diagnostics_->publish())
       stopService();
 
-    manager_->update(time, ros::Duration(1.0f/controller_freq_));
+    readJoints();
+
+    //motion_->stiffnessInterpolation(diagnostics_->getForcedJoints(), 0.3f, 2.0f);
+
+    try
+    {
+      manager_->update(time, ros::Duration(1.0f/controller_freq_));
+    }
+    catch(ros::Exception& e)
+    {
+      ROS_ERROR("%s", e.what());
+      return;
+    }
 
     writeJoints();
 
-    publishJointStateFromAlMotion();
+    //no need if Naoqi Driver is running
+    //publishJointStateFromAlMotion();
 
     rate.sleep();
   }
@@ -342,20 +412,50 @@ void Robot::publishBaseFootprint(const ros::Time &ts)
                                                                  base_link_frame, "base_footprint"));
 }
 
-void Robot::readJoints()
+std::vector <bool> Robot::checkJoints()
 {
-  //read memory keys
-  std::vector<float> joint_positions = memory_->getListData();
+  std::vector <bool> hw_enabled;
+  hw_enabled.reserve(hw_joints_.size());
+  hw_enabled.resize(hw_joints_.size(), false);
 
   //store joints angles
-  std::vector <double>::iterator it_command = joint_commands_.begin();
-  std::vector <double>::iterator it_now = joint_angles_.begin();
-  std::vector <float>::iterator it_sensor = joint_positions.begin();
-  for(; it_command<joint_commands_.end(); ++it_command, ++it_now, ++it_sensor)
+  std::vector<std::string>::iterator hw_j = hw_joints_.begin();
+  std::vector<std::string>::iterator qi_j = qi_joints_.begin();
+  std::vector<bool>::iterator hw_enabled_j = hw_enabled.begin();
+
+  for(; hw_j != hw_joints_.end(); ++hw_j, ++hw_enabled_j)
   {
-    *it_now = *it_sensor;
+    if (*hw_j == *qi_j)
+    {
+      *hw_enabled_j = true;
+      ++qi_j;
+    }
+  }
+  return hw_enabled;
+}
+
+void Robot::readJoints()
+{
+  //read memory keys for joint/position/sensor
+  std::vector<float> qi_joints_positions = memory_->getListData();
+
+  //store joints angles
+  std::vector<double>::iterator hw_command_j = hw_commands_.begin();
+  std::vector<double>::iterator hw_angle_j = hw_angles_.begin();
+  std::vector<float>::iterator qi_position_j = qi_joints_positions.begin();
+  std::vector<bool>::iterator hw_enabled_j = hw_enabled_.begin();
+
+  for(; hw_command_j != hw_commands_.end(); ++hw_command_j, ++hw_angle_j, ++hw_enabled_j)
+  {
+    if (!*hw_enabled_j)
+      continue;
+
+    *hw_angle_j = *qi_position_j;
     // Set commands to the read angles for when no command specified
-    *it_command = *it_sensor;
+    *hw_command_j = *qi_position_j;
+
+    //increment qi iterators
+    ++qi_position_j;
   }
 }
 
@@ -373,11 +473,19 @@ void Robot::writeJoints()
 {
   // Check if there is some change in joints values
   bool changed(false);
-  std::vector<double>::iterator it_now = joint_angles_.begin();
-  std::vector<double>::iterator it_command = joint_commands_.begin();
-  for(int i=0; it_command != joint_commands_.end(); ++i, ++it_command, ++it_now)
+  std::vector<double>::iterator hw_angle_j = hw_angles_.begin();
+  std::vector<double>::iterator hw_command_j = hw_commands_.begin();
+  std::vector<double>::iterator qi_command_j = qi_commands_.begin();
+  std::vector<bool>::iterator hw_enabled_j = hw_enabled_.begin();
+  for(int i=0; hw_command_j != hw_commands_.end(); ++i, ++hw_command_j, ++hw_angle_j, ++hw_enabled_j)
   {
-    double diff = fabs(*it_command - *it_now);
+    if (!*hw_enabled_j)
+      continue;
+
+    *qi_command_j = *hw_command_j;
+    ++qi_command_j;
+
+    double diff = std::fabs(*hw_command_j - *hw_angle_j);
     if(diff > joint_precision_)
     {
       //ROS_INFO_STREAM(" joint " << i << " : diff=" << diff);
@@ -391,9 +499,9 @@ void Robot::writeJoints()
     return;
 
   if (use_dcm_)
-    dcm_->writeJoints(joint_commands_);
+    dcm_->writeJoints(qi_commands_);
   else
-    motion_->writeJoints(joint_commands_);
+    motion_->writeJoints(qi_commands_);
 }
 
 void Robot::ignoreMimicJoints(std::vector <std::string> *joints)
@@ -415,7 +523,7 @@ bool Robot::setStiffness(const float &stiffness)
 {
   stiffness_.data = stiffness;
 
-  if (!motion_->stiffnessInterpolation(motor_groups_, stiffness, 1.0f))
+  if (!motion_->stiffnessInterpolation(motor_groups_, stiffness, 2.0f))
     return false;
 
   return true;
